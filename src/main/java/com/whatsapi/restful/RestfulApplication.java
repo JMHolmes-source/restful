20e2045d62d49437f3f09ffa70d16b7bc5db1280
Mick Holmes <joshua.holmes@bbd.co.za>
2025-03-10 14:03:46 +0200

    Updated some kak and now it runs ig

--- src/main/java/com/whatsapi/restful/RestfulApplication.java
+++ src/main/java/com/whatsapi/restful/RestfulApplication.java
@@ -1,10 +1,19 @@
 package com.whatsapi.restful;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class RestfulApplication {
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(RestfulApplication.class, args);
 	}
