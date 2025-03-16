bfa51d989345af05786202d045274370c7376a36
JMHolmes-Source <joshua.holmes@bbd.co.za>
2025-03-16 21:12:23 +0200

    test

--- src/main/java/com/whatsapi/restful/controllers/conversationController.java
+++ src/main/java/com/whatsapi/restful/controllers/conversationController.java
@@ -10,17 +10,14 @@ import com.whatsapi.restful.models.Message;
 @RestController
 @RequestMapping("/api/conversation")
 public class ConversationController {
+
     @GetMapping("/{id}")
     Chat conversation() {
         Chat temp = new Chat();
-        temp.addMessage(
-                new Message("yaosile", "I'm doing well, thanks! What are you up to today?", "2023-10-01", "10:02:00"));
-        temp.addMessage(
-                new Message("greggyweggy", "Just relaxing. Might go for a walk later. You?", "2023-10-01", "10:03:00"));
-        temp.addMessage(new Message("yaosile", "Nice! I’ve got some work to finish, but I might take a break later.",
-                "2023-10-01", "10:04:00"));
-        temp.addMessage(
-                new Message("greggyweggy", "Sounds like a plan. Don’t overwork yourself!", "2023-10-01", "10:05:00"));
+        temp.addMessage(new Message("yaosile", "I'm doing well, thanks! What are you up to today?", "2023-10-01", "10:02:00"));
+        temp.addMessage(new Message("greggyweggy", "Just relaxing. Might go for a walk later. You?", "2023-10-01", "10:03:00"));
+        temp.addMessage(new Message("yaosile", "Nice! I’ve got some work to finish, but I might take a break later.","2023-10-01", "10:04:00"));
+        temp.addMessage(new Message("greggyweggy", "Sounds like a plan. Don’t overwork yourself!", "2023-10-01", "10:05:00"));
         temp.addMessage(new Message("yaosile", "Haha, I’ll try. Enjoy your walk!", "2023-10-01", "10:06:00"));
         temp.addMessage(new Message("greggyweggy", "Thanks! Talk later!", "2023-10-01", "10:07:00"));
 
