4410d59f1d9a182e5da45c737e607664c21d8ab7
JMHolmes-Source <joshua.holmes@bbd.co.za>
2025-03-16 15:17:15 +0200

    test

--- src/main/java/com/whatsapi/restful/models/Message.java
+++ src/main/java/com/whatsapi/restful/models/Message.java
@@ -2,7 +2,7 @@ package com.whatsapi.restful.models;
 
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
-@JsonSerialize
+// @JsonSerialize
 public class Message {
     private int userID;
     private int recipient;
