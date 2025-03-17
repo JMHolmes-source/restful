620a624c8dedfe78e2b657bb115cef1a29591883
Mick Holmes <joshua.holmes@bbd.co.za>
2025-03-17 11:11:53 +0200

    test

--- src/main/java/com/whatsapi/restful/models/User.java
+++ src/main/java/com/whatsapi/restful/models/User.java
@@ -7,12 +7,8 @@ import javax.persistence.Id;
 import lombok.Data;
 
 @Data
-@Entity
 public class User{
-    @Id
     String username;
-
-    @Basic
     String useremail;
     public User(String username, String useremail) {
         this.username = username;
