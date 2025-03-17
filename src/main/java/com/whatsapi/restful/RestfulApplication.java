689e75c022a6492d8f6d613e8bb915fcd1bc9833
Mick Holmes <joshua.holmes@bbd.co.za>
2025-03-17 15:31:02 +0200

    test

--- src/main/java/com/whatsapi/restful/RestfulApplication.java
+++ src/main/java/com/whatsapi/restful/RestfulApplication.java
@@ -3,11 +3,16 @@ package com.whatsapi.restful;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
+@EnableJpaRepositories("com.whatsapi.restful.*")
+@EntityScan("com.whatsapi.restful.*")
 public class RestfulApplication {
 
 	@RequestMapping("/test")
