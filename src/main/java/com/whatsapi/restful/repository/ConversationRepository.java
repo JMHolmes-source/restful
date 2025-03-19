41df96b7a9e41eb6b31ba68e937066544e74aa15
Mick Holmes <joshua.holmes@bbd.co.za>
2025-03-19 16:29:29 +0200

    test

--- src/main/java/com/whatsapi/restful/repository/ConversationRepository.java
+++ src/main/java/com/whatsapi/restful/repository/ConversationRepository.java
@@ -1,19 +1,17 @@
 package com.whatsapi.restful.repository;
 
-import com.whatsapi.restful.models.User;
+import com.whatsapi.restful.models.Conversation;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import java.time.*;
 
 @Repository
-public interface ConversationRepository extends JpaRepository<User, Long> {
-    @Modifying
-    @Transactional
+public interface ConversationRepository extends JpaRepository<Conversation, Long> {
+    // @Modifying
+    // @Transactional
     @Query(nativeQuery = true, value = "INSERT INTO public.conversations (conversation_name ,created_at) VALUES(:conversationName, NOW()) returning conversation_id;")
-    int createConversation(
-            @Param("conversationName") String conversationName);
+    int createConversation(@Param("conversationName") String conversationName);
 }
